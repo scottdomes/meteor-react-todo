e1147385a3d12cd969204da6a4fffe2d9ed30e91
scottdomes <scottdomes@gmail.com>
2016-04-02 14:04:19 -0700

    Added ability to check off and delete tasks

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -56,6 +56,6 @@ App.proptypes = {
 
 export default createContainer(() => {
   return {
-    tasks: Tasks.find({}).fetch(),
+    tasks: Tasks.find({}, { sort: { createdAt: -1 } }).fetch(),
   };
 }, App);
